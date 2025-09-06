ee1f745c247db75f78973a5d4ee39aef6b928bc6
Yhumi <yhumi@miyei.me>
2025-09-06 22:02:41 +0100

    Initial nix pico template project using flake.nix and direnv.

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "pico/stdio.h"
+
+int main(void) {
+    stdio_init_all();
+    
+    while (true) {
+        printf("Hello world.");
+        sleep_ms(1000);
+    }
+}printf("Hello world.");
